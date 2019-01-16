ce218e239848ad5196114f64deb8d9006bb55596
Andrew Shi <andrew@syntechcorporation.com>
2019-01-15 20:59:38 -0800

    Use munkyshi/autoflake-v1.3.dev0

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['autoflake==1.1'],
+    install_requires=['autoflake==v1.3.dev0'],
+    dependency_links=[
+        "https://munkyshi.github.io/python-packages/autoflake",
+    ],
 )
