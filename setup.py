146293e068d3a85d5d831645fa8e7a8fff2debb2
Andrew Shi <andrew@syntechcorporation.com>
2019-01-15 21:12:43 -0800

    plop

--- setup.py
+++ setup.py
@@ -5,7 +5,5 @@ setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
     install_requires=['autoflake==1.3-munkyshi'],
-    dependency_links=[
-        'git+ssh://git@github.com/munkyshi/autoflake.git#egg=autoflake-1.3-munkyshi',
-    ]
+    dependency_links=['http://github.com/munkyshi/autoflake/tarball/master#egg=autoflake-1.3-munkyshi']
 )
